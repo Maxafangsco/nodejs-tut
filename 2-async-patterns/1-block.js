9104ef35b6a8e41d16f3e6f777e2e97e5d4ff8c3
maxafangsco <maxafangsco@gmail.com>
2022-02-14 10:03:30 +0400

    intro to nodejs completed

---
+++ 2-async-patterns/1-block.js
@@ -0,0 +1,25 @@
+const http = require('http')
+
+
+const server = http.createServer((req,res)=>{
+if(req.url==='/'){
+    res.end('Home page')
+}else if(req.url==='/about'){
+
+    //Blocking code
+
+    for(let i =0; i<1000; i++){
+        for(let j =0; j<1000; j++){
+            console.log(`${i } and ${j}`)
+        }
+    }
+    res.end('About page')
+}
+else{
+    res.end('Error page')
+}
+})
+
+server.listen(5000,()=>{
+    console.log('server is Listerning for port 5000...')
+})
